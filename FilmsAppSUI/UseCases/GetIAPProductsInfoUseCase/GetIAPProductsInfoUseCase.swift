5cb218cdd1a3c19988924866a1909a0c86f32567
Виталий Баник <cefiro666@mail.ru>
2020-10-27 14:58:52 +0700

    fix

--- FilmsAppSUI/UseCases/GetIAPProductsInfoUseCase/GetIAPProductsInfoUseCase.swift
+++ FilmsAppSUI/UseCases/GetIAPProductsInfoUseCase/GetIAPProductsInfoUseCase.swift
@@ -7,14 +7,24 @@
 //
 
 import SQStoreKit
+import Foundation
 
 // MARK: - GetFilmsUseCaseImpl
 class GetIAPProductsInfoUseCaseImpl: GetIAPProductsInfoUseCase {
     
 // MARK: - Methods
     func execute(_ completionHandler: @escaping (_ success: Bool, _ productsInfo: IAPProductsInfo?, _ errorMessage: String?) -> ()) {
-        let productsInfo = IAPProductsInfo(productsIdentifiers: ["oneScreenshootProtocol"], sharedSecret: nil)
-        completionHandler(true, productsInfo, nil)
+        if let bundle = Bundle.main.bundleIdentifier {
+            let IAPBundles = [
+                "oneScreenshootProtocol"
+            ]
+            
+            let productsInfo = IAPProductsInfo(productsIdentifiers: IAPBundles.map { "\(bundle).\($0)" }, sharedSecret: nil)
+            completionHandler(true, productsInfo, nil)
+            return
+        }
+        
+        completionHandler(false, nil, "Invalid bundle!")
     }
     
 }
